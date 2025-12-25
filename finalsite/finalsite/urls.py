1e73ac7fb26b5e1644e7c0446f703e0a5aa4a06b
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-12-25 21:50:03 +0800

    added the .giticore

--- finalsite/finalsite/urls.py
+++ finalsite/finalsite/urls.py
@@ -6,9 +6,10 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path #added so that the image will be loaded to site
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # added serve
 
 urlpatterns = [
     path('admin/', admin.site.urls), #connect to admin py
@@ -19,3 +20,8 @@ urlpatterns = [
 # during local development (when settings.DEBUG is True).
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
