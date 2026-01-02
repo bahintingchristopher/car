4b09026481f67c5d6543eae322a39ebfcb268ca7
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-01-02 17:14:56 +0800

    modify the build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,17 @@ python manage.py collectstatic --no-input
 # 3. Run migrations
 python manage.py migrate
 
-# 4. Create Admin (Superuser)
-python manage.py shell -c "import os; from django.contrib.auth.models import User; u = os.environ.get('SUPERUSER_NAME'); e = os.environ.get('SUPERUSER_EMAIL'); p = os.environ.get('SUPERUSER_PASSWORD'); User.objects.filter(username=u).exists() or User.objects.create_superuser(u, e, p)"
+# 4. Create/Update Admin (The Fail-Safe Way)
+python manage.py shell -c "from django.contrib.auth.models import User; import os; \
+username = os.environ.get('SUPERUSER_NAME'); \
+email = os.environ.get('SUPERUSER_EMAIL'); \
+password = os.environ.get('SUPERUSER_PASSWORD'); \
+user, created = User.objects.get_or_create(username=username, defaults={'email': email}); \
+user.set_password(password); \
+user.is_staff = True; \
+user.is_superuser = True; \
+user.save(); \
+print('ADMIN USER UPDATED/CREATED SUCCESSFULLY')"
 
 # 5. Create Guest (Regular User)
 python manage.py shell -c "import os; from django.contrib.auth.models import User; u = os.environ.get('GUEST_USER_NAME'); e = os.environ.get('GUEST_USER_EMAIL'); p = os.environ.get('GUEST_USER_PASSWORD'); User.objects.filter(username=u).exists() or User.objects.create_user(u, e, p)"
