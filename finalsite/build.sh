9510980b39e9537c2e12610638249decef3b91c7
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-01-02 18:04:31 +0800

    add the guest attributes to view  false or view only

--- finalsite/build.sh
+++ finalsite/build.sh
@@ -11,8 +11,14 @@ python manage.py collectstatic --no-input
 # 3. Run migrations
 python manage.py migrate
 
-# 4. Create/Update Admin (One single line for safety)
+# 4. Create/Update Admin
 python manage.py shell -c "from django.contrib.auth.models import User; import os; u=os.environ.get('SUPERUSER_NAME'); e=os.environ.get('SUPERUSER_EMAIL'); p=os.environ.get('SUPERUSER_PASSWORD'); user, created = User.objects.get_or_create(username=u, defaults={'email': e}); user.set_password(p); user.is_staff=True; user.is_superuser=True; user.save(); print(f'>>> ADMIN STATUS: User {u} is ready')"
 
-# 5. Create Guest User (One single line)
-python manage.py shell -c "from django.contrib.auth.models import User; import os; u=os.environ.get('GUEST_USER_NAME'); e=os.environ.get('GUEST_USER_EMAIL'); p=os.environ.get('GUEST_USER_PASSWORD'); User.objects.filter(username=u).exists() or User.objects.create_user(u, e, p); print('>>> GUEST STATUS: Guest user ready')"
+# 5. Create Guest (As Staff so they can log in, but NOT superuser)
+python manage.py shell -c "from django.contrib.auth.models import User; import os; u=os.environ.get('GUEST_USER_NAME'); e=os.environ.get('GUEST_USER_EMAIL'); p=os.environ.get('GUEST_USER_PASSWORD'); user, created = User.objects.get_or_create(username=u, defaults={'email': e}); user.set_password(p); user.is_staff=True; user.is_superuser=False; user.save(); print('>>> GUEST STATUS: Guest user ready')"
+
+# 6. Automate View-Only Group and Assign Guest
+python manage.py shell -c "from django.contrib.auth.models import User, Group, Permission; import os; group, _ = Group.objects.get_or_create(name='Viewers'); perms = Permission.objects.filter(codename__contains='view'); group.permissions.set(perms); u_name=os.environ.get('GUEST_USER_NAME'); user = User.objects.get(username=u_name); user.groups.add(group); print('>>> PERMISSIONS: Guest assigned to Viewers group')"
+
+# 7. Load Car Data (Keep this at the end)
+python manage.py load_car_data
